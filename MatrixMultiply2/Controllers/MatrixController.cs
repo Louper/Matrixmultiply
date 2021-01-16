bf8e70263b2f0285c8bf8b8d947ba0ca3e0806a2
Oswaldo Antonio Garay Benavides <oswaldo.garay@inalambria.com>
2021-01-16 14:52:31 -0500

    Version Definitiva Back

---
+++ MatrixMultiply2/Controllers/MatrixController.cs
@@ -0,0 +1,45 @@
+﻿using MatrixMultiply2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MatrixMultiply2.Controllers
+{
+    public class MatrixController : ApiController
+    {
+        [HttpPost]
+
+        public double[,] Sum(MatrixStorage storage)
+        {
+
+            int rows1 = storage.FirstMatrix.GetLength(0);
+            int columns1 = storage.FirstMatrix.GetLength(1);
+            int rows2 = storage.SecondMatrix.GetLength(0);
+            int columns2 = storage.SecondMatrix.GetLength(1);
+
+            if (columns1 == rows2)
+            {
+                double[,] product = new double[rows1, columns2];
+                for (int i = 0; i < rows1; i++)
+                {
+                    // for each matrix 1 row, loop through matrix 2 columns  
+                    for (int j = 0; j < columns2; j++)
+                    {
+                        // loop through matrix 1 columns to calculate the dot product  
+                        for (int z = 0; z < columns1; z++)
+                        {
+                            product[i, j] += storage.FirstMatrix[i, z] * storage.SecondMatrix[z, j];
+                        }
+                    }
+                }
+                return product;
+            }
+            else
+            {
+                throw new InvalidOperationException("Remember the number of Rows on first Matrix must coincide with number of Columns on second Matrix! Check and try again!");
+            }
+        }
+    }
+}
---
+++ MatrixMultiply2/Models/MatrixStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatrixMultiply2.Models
+{
+    public class MatrixStorage
+    {
+        public double[,] FirstMatrix{ get; set; }
+
+        public double[,] SecondMatrix { get; set; }
+    }
+}
